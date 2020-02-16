6b07a1edc24310dc0e48603a34047d7cfb9702c7
helvis <helvis.eloy1998@gmail.com>
2020-02-16 11:02:38 -0400

    primera version de prueba

---
+++ probando_libreria.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "suma.h"
+using namespace std;
+
+int main(){
+	cout<<"hola mundo\n";
+	cout<<suma (3,5);
+}
---
+++ suma.h
@@ -0,0 +1,6 @@
+#include <iostream>
+int suma (int n,int m){
+	int res=0;
+	res=n+m;
+	return res;
+}
